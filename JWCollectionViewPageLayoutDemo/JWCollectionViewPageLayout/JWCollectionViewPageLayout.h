6f5df4d7679ac46febf7afdf22ad8b9f404a37f2
Javenfly <960838547@qq.com>
2018-01-09 19:57:41 +0800

    编辑JWCollectionViewPageLayout

--- JWCollectionViewPageLayoutDemo/JWCollectionViewPageLayout/JWCollectionViewPageLayout.h
+++ JWCollectionViewPageLayoutDemo/JWCollectionViewPageLayout/JWCollectionViewPageLayout.h
@@ -7,7 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+@class JWCollectionViewPageLayout;
+@protocol JWCollectionViewDelegatePageLayOut <UICollectionViewDelegate>
+@optional
+- (CGSize)jw_collectionView:(UICollectionView *)collectionView layout:(JWCollectionViewPageLayout*)collectionViewLayout sizeForItemAtSection:(NSInteger)section;
 
+- (UIEdgeInsets)jw_collectionView:(UICollectionView *)collectionView layout:(JWCollectionViewPageLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section;
+
+- (CGFloat)jw_collectionView:(UICollectionView *)collectionView layout:(JWCollectionViewPageLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section;
+
+- (CGFloat)jw_collectionView:(UICollectionView *)collectionView layout:(JWCollectionViewPageLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section;
+@end
 @interface JWCollectionViewPageLayout : UICollectionViewLayout
+
+/** item的大小 */
+@property (nonatomic, assign) CGSize itemSize;
+/** section的内边距 */
+@property (nonatomic, assign) UIEdgeInsets sectionInsets;
+/** 最小行间距 */
+@property (nonatomic, assign) CGFloat minimumLineSpacing;
+/** 最小item间隔 */
+@property (nonatomic, assign) CGFloat minimunInteritemSpacing;
 
 @end
